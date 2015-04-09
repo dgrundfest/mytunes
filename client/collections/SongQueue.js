6a1987ea47ec765e78b257c83e9fd7ae51a00f5c
dgrundfest <dave4@grundfest.com>
2015-04-08 17:29:58 -0700

    Working through tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,35 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add',function(song){
+      if(this.length === 1){
+        this.playFirst();
+      }
+    },this);
+
+    // this.on('all', function(eventname) {
+    //   console.log(eventname);
+    //   // this.remove(song);
+    // }, this);
+
+    this.on('ended', function(song) {
+      this.remove(song);
+      this.playFirst();
+    }, this);
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+      this.playFirst();
+    }, this);
+  },
+
+  playFirst : function(){
+    if (this.at(0)) {
+      this.at(0).play();
+    }
   }
 
+
+
 });
+
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,14 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
+  tagName: "table",
 
   initialize: function() {
   },
 
   render: function() {
-    return this.$el;
+    return this.$el.append(this.collection.map(function(song) {
+      return new SongQueueEntryView({model: song});
+    }));
   }
 
 });
